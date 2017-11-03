d09027d05e5475b19288e7f8f96f81d83fca77bb
JGrindal <github@jgrindal.com>
2017-11-03 02:56:18 -0500

    Added basic crypto
    Added basic crypto functionality (blatantly ripped from cryptography's Fernet)

--- src/client.py
+++ src/client.py
@@ -1,25 +1,28 @@
 '''Chat client'''
 
-import sys
-import socket
-import select
-
 from clientgui import *
 from crypto import *
 
 class Client:
+    app = None
+    f = None
+
     def quit(self):
         exit()
 
 
-    def send(self, message):
+    def send(self, message=None):
         bytecode = str.encode(message)
+        key = Potamus.generate_key()
+        f = Potamus(key)
+        ciphertext = f.encrypt(bytecode)
+        self.app.showcipher(ciphertext)
 
 
     def main(self):
         root = Tk()
         root.geometry("400x150")
-        app = MessageWindow(root, self)
+        self.app = MessageWindow(root, self)
         root.mainloop()
 
 
--- src/clientgui.py
+++ src/clientgui.py
@@ -1,9 +1,12 @@
-from tkinter import *
+'''Test fixture GUI for client'''
 
+from tkinter import *
+from tkinter import messagebox
 
 class MessageWindow(Frame):
     ownerClient = None
-    message = None
+    text_message = None
+
 
     def __init__(self, master=None, client=None):
         Frame.__init__(self, master)
@@ -14,8 +17,8 @@ class MessageWindow(Frame):
     def init_window(self):
         self.master.title("GUI")
         self.pack(fill=BOTH, expand=1)
-        text_message = Text(self, height=3, textvariable=self.message)
-        text_message.pack()
+        self.text_message = Text(self, height=3)
+        self.text_message.pack()
         button_send = Button(self, text="Send", command=self.client_send)
         button_send.pack()
         button_quit = Button(self, text="Quit", command=self.client_exit)
@@ -25,4 +28,7 @@ class MessageWindow(Frame):
         self.ownerClient.quit()
 
     def client_send(self):
-        self.ownerClient.send(self.message)
+        self.ownerClient.send(self.text_message.get("1.0", "end-1c"))
+
+    def showcipher(self, ciphertext):
+        messagebox.showinfo("Ciphertext", ciphertext)
